4ab10031a4f71e7db63426d6de1010d252af46fa
tanuj <tanujbhatt644@gmail.com>
2024-05-31 00:24:05 +0530

    first commit

---
+++ App.py
@@ -0,0 +1,138 @@
+import streamlit as st
+import nltk
+import spacy
+nltk.download('stopwords')
+spacy.load('en_core_web_sm')
+
+import pandas as pd
+import base64, random
+import time, datetime
+from pyresparser import ResumeParser
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+import io, random
+from streamlit_tags import st_tags
+from PIL import Image
+import pymysql
+import pafy
+import plotly.express as px
+import youtube_dl
+
+
+
+
+
+def get_table_download_link(df, filename, text):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
+    return href
+
+
+def pdf_reader(file):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    with open(file, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+            print(page)
+        text = fake_file_handle.getvalue()
+
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+    return text
+
+
+def show_pdf(file_path):
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+
+
+st.set_page_config(
+    page_title="ProfilePro: Resume Analyzer",
+)
+
+
+def run():
+    st.title("ProfilePro: Resume Analyzer")
+    
+
+
+    pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+    if pdf_file is not None:
+        save_image_path = './Uploaded_Resumes/' + pdf_file.name
+        with open(save_image_path, "wb") as f:
+            f.write(pdf_file.getbuffer())
+        show_pdf(save_image_path)
+        resume_data = ResumeParser(save_image_path).get_extracted_data()
+        if resume_data:
+                ## Get the whole resume data
+                resume_text = pdf_reader(save_image_path)
+
+
+                # Calculating  score
+                resume_score = 0
+                if 'Objective' in resume_text:
+                    resume_score = resume_score + 20
+                   
+
+                if 'Declaration' in resume_text:
+                    resume_score = resume_score + 20
+                   
+
+                if 'Hobbies' or 'Interests' in resume_text:
+                    resume_score = resume_score + 20
+                    
+
+                if 'Achievements' in resume_text:
+                    resume_score = resume_score + 20
+                  
+
+                if 'Projects' in resume_text:
+                    resume_score = resume_score + 20
+                    
+
+                st.subheader("**Resume ATS Score**")
+                st.markdown(
+                    """  
+                    <style>
+                        .stProgress > div > div > div > div {
+                            background-color: #d73b5c;
+                        }
+                    </style>""",
+                    unsafe_allow_html=True,
+                )
+                my_bar = st.progress(0)
+                score = 0
+                for percent_complete in range(resume_score):
+                    score += 1
+                    time.sleep(0.1)
+                    my_bar.progress(percent_complete + 1)
+                st.success('** Your Resume Writing Score: ' + str(score) + '**')
+                st.warning(
+                    "** Note: This score is calculated based on the content that you have added in your Resume. **")
+                st.balloons()
+
+
+                
+        else:
+                st.error('Something went wrong..')
+  
+
+
+run()
